59a1d157b34b15fcb16cdad13a821fe99d166b73
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-04-11 17:48:58 +0500

    Add postfix expression evaluation function
    Implemented a function to evaluate postfix expressions using a stack.

--- Stack/EvaluatePostFix.cpp
+++ Stack/EvaluatePostFix.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void solve(string exp){
+    stack<int> st; 
+
+    for(int i = 0; i < exp.length(); i++){
+        if(exp[i] == ' '){
+            continue; 
+        } else if(isdigit(exp[i])){
+            st.push(exp[i] - '0'); 
+        } else {
+            int first = st.top(); 
+            st.pop();
+            int sec = st.top();   
+            st.pop();
+
+            switch(exp[i]){
+                case '+': st.push(sec + first); break;
+                case '-': st.push(sec - first); break;
+                case '*': st.push(sec * first); break;
+                case '/': st.push(sec / first); break;
+            }
+        }
+    }
+
+    cout << "Answer: " << st.top() << endl;
+}
+
+int main(){
+    string exp = "5 6 2 + * 12 4 / -"; // postfix expression
+    solve(exp);
+    return 0;
+}
