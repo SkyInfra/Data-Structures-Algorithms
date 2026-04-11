1afeaae354ae7b54b91309b91d2bdb86e631a954
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-04-11 17:44:31 +0500

    Add postfix conversion function for expressions
    Implement function to convert infix expressions to postfix.

--- Stack/convertIntoPostfix.cpp
+++ Stack/convertIntoPostfix.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <stack>
+using namespace std;
 
+
+
+string convertIntoPostfix(string exp){
+    string res = "";
+    stack<char> st;
+
+    for(char ch : exp){
+        if(isOperand(ch)){
+            res += ch;
+        } else if(ch == '('){
+            st.push(ch);
+        } else if(ch == ')'){
+            while(!st.empty() && st.top() != '('){
+                res += st.top();
+                st.pop();
+            }
+            if(!st.empty()) st.pop(); 
+        } else {
+            while(!st.empty() && precedence(st.top()) >= precedence(ch)){
+                res += st.top();
+                st.pop();
+            }
+            st.push(ch);
+        }
+    }
+
+    while(!st.empty()){
+        res += st.top();
+        st.pop();
+    }
+
+    return res;
+}
+
+int main(){
+    string exp = "A+B*C-D";
+    string output = convertIntoPostfix(exp);
+    cout << "Postfix: " << output << endl;
+    return 0;
+}
