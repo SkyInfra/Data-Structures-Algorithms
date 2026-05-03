a8c1aa71ab8f36ab837ae1f3cf360adaf5253dba
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-03 13:14:31 +0500

    Add Node structure for level order traversal

--- BST/BST_Traversal_using_levelOrder.cpp
+++ BST/BST_Traversal_using_levelOrder.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<queue>
+using namespace std;
 
+struct Node{
+    int value;
+    Node* left;
+    Node* right;
+
+    Node(int v){
+        value = v;
+        left = NULL;
+        right = NULL;
+    }
+};
