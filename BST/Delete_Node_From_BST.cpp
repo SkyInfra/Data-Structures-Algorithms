cf43e7db941a917000826b6def7e2f539f749c9d
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-03 13:16:17 +0500

    Correct Node struct and include directives
    Fix syntax errors in Node struct initialization.

--- BST/Delete_Node_From_BST.cpp
+++ BST/Delete_Node_From_BST.cpp
@@ -1,1 +1,17 @@
 
+#include<iostream>
+using namespace std;
+#include <iostream>
+#include<queue>
+using namespace std;
+struct Node {
+    int value ;
+    Node* left ;
+    Node* right;
+
+    Node(int v){
+        this->value =v;
+        this->left = NULL;
+        this-right = NULL;
+    }
+};
