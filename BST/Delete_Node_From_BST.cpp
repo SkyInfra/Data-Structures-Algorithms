93c27e4117762a0a84f51f2ca417d08a0e397584
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-03 13:16:40 +0500

    Add delete node function to BST
    Implement delete functionality for nodes in a binary search tree.

--- BST/Delete_Node_From_BST.cpp
+++ BST/Delete_Node_From_BST.cpp
@@ -15,3 +15,42 @@ struct Node {
         this-right = NULL;
     }
 };
+class Tree{
+    Node* root;
+    Tree(){
+        root = nullptr;
+    }
+    Node* finMind(Node* root , int key){
+        while(root != NULL && root->left != NULL){
+            root = root->left ;
+        }
+        return root ;
+    }
+    Node* delete_Node(Node* root , int key){
+        if(root == NULL){
+            return ;
+        }
+        if(key < root->value){
+            root->left = delete_Node(root->left , key);
+        }else{
+            root->right = delete_Node(root->right, key);
+        }else{
+            if(root->left == NULL && root->right == NULL){
+                delete root;
+                return;
+            }else if(root->left == NULL){
+                Node* temp = root->right ;
+                delete temp;
+                return;
+            }else if(temp->right == NULL){
+                Node* temp = root->left ;
+                delete temp;
+                return;
+            }
+            Node* temp = finMind(root->right);
+            root->value = temp->value ;
+            root->right = delete_Node(root->right);
+        }
+        return root;
+    }
+}
