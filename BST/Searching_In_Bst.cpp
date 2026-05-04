c6f882ef67131429760f410597a860845ad16d79
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-04 22:41:56 +0500

    Implement Tree class with recursive search method
    Added a Tree class with a manually created BST and a recursive search method.

--- BST/Searching_In_Bst.cpp
+++ BST/Searching_In_Bst.cpp
@@ -12,3 +12,33 @@ struct Node{
         right = NULL;
     }
 };
+class Tree{
+private:
+    Node* root;
+
+public:
+    Tree(){
+        // Manually creating a BST
+        root = new Node(10);
+        root->left = new Node(5);
+        root->right = new Node(15);
+        root->left->left = new Node(3);
+        root->left->right = new Node(7);
+    }
+
+    // Recursive Search
+    bool search_recursive(Node* root, int v){
+        if(root == NULL){
+            return false;
+        }
+
+        if(root->value == v){
+            return true;
+        }
+        else if(v < root->value){
+            return search_recursive(root->left, v);
+        }
+        else{
+            return search_recursive(root->right, v);
+        }
+    }
