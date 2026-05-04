2afa4fd30f1751844bd012fc1bb309784143d2df
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-04 22:41:31 +0500

    Add Node structure for BST implementation

--- BST/Searching_In_Bst.cpp
+++ BST/Searching_In_Bst.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
 
+struct Node{
+    int value;
+    Node* left;
+    Node* right;
+
+    Node(int v){
+        value = v;
+        left = NULL;
+        right = NULL;
+    }
+};
