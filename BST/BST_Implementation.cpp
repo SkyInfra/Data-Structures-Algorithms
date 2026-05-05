a8a0b1774fcfc239c4b3b06ef934c0a1e898ccfe
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-05 19:28:28 +0500

    Add preOrder, postOrder, and search functions

--- BST/BST_Implementation.cpp
+++ BST/BST_Implementation.cpp
@@ -40,6 +40,72 @@ public:
         {
             root->right = add_Node(root->right, v);
         }
+         void preOrder(Node *root)
+    {
+        if (root == NULL)
+            return;
 
+        cout << root->value << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+
+    void postOrder(Node *root)
+    {
+        if (root == NULL)
+            return;
+
+        postOrder(root->left);
+        postOrder(root->right);
+        cout << root->value << " ";
+    }
+    Node *search(Node *root, int key)
+    {
+        if (root == NULL)
+        {
+            return NULL;
+        }
+
+        Node *temp = root;
+
+        while (temp != NULL)
+        {
+            if (temp->value == key)
+            {
+                return temp;
+            }
+            else if (key < temp->value)
+            {
+                temp = temp->left;
+            }
+            else
+            {
+                temp = temp->right;
+            }
+        }
+
+        return NULL;
+    }
+    Node *search_list(Node *root, int key)
+    {
+        if (root == NULL)
+        {
+            return NULL;
+        }
+
+        if (root->value == key)
+        {
+            return root;
+        }
+
+        if (key < root->value)
+        {
+            return search_list(root->left, key);
+        }
+        else
+        {
+            return search_list(root->right, key);
+        }
+    }
         return root;
     }
