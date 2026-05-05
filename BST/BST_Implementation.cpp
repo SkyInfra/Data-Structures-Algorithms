e0ce5c9a261de188d56226c0f6048b890f9af02c
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-05 19:27:53 +0500

    Add binary search tree implementation
    Implemented a binary search tree with node insertion functionality.

--- BST/BST_Implementation.cpp
+++ BST/BST_Implementation.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+struct Node
+{
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int v)
+    {
+        value = v;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Tree
+{
+    Node *root;
+
+public:
+    Tree()
+    {
+        root = NULL;
+    }
+
+    Node *add_Node(Node *root, int v)
+    {
+        if (root == NULL)
+        {
+            return new Node(v);
+        }
+
+        if (v < root->value)
+        {
+            root->left = add_Node(root->left, v);
+        }
+        else
+        {
+            root->right = add_Node(root->right, v);
+        }
+
+        return root;
+    }
