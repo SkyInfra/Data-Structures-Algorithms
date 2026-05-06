5d8a889e0c8dd97aab7cb2b6d17e4002c99a5c19
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-07 00:33:30 +0500

    Add Edge and Node structures for graph
    Define Edge and Node structures for graph representation.

--- Graph/Grapgh_using_Adjacent_list.cpp
+++ Graph/Grapgh_using_Adjacent_list.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
 
+struct Edge {
+    int value;
+    Edge* next;
+
+    Edge(int v) {
+        value = v;
+        next = NULL;
+    }
+};
+
+struct Node {
+    int value;
+    Edge* head;
+    Edge* tail;
+    Node* next;
+
+    Node(int v) {
+        value = v;
+        head = tail = NULL;
+        next = NULL;
+    }
+};
