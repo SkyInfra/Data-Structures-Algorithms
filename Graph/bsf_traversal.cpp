6d84ad9bed1def38fdce6d12deb8a10186487473
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-06 19:35:07 +0500

    Add main function for BFS traversal example

--- Graph/bsf_traversal.cpp
+++ Graph/bsf_traversal.cpp
@@ -24,3 +24,21 @@ void traverse(int start, vector<vector<int>> &adj, vector<bool> &visited) {
         }
     }
 }
+int main() {
+    int vertices = 5;
+
+    vector<vector<int>> adj(vertices);
+
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    vector<bool> visited(vertices, false);
+
+    cout << "BFS Traversal: ";
+    traverse(0, adj, visited);
+
+    return 0;
+}
