8f7e376eaacd2ab38ab10f4f3b3b2ac3056772cc
Haseeb Akhtar <haseeb.dealsphere@gmail.com>
2026-05-06 19:34:41 +0500

    Add BFS traversal function for graph
    Implement BFS traversal for graph using a queue.

--- Graph/bsf_traversal.cpp
+++ Graph/bsf_traversal.cpp
@@ -1,1 +1,26 @@
 
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+void traverse(int start, vector<vector<int>> &adj, vector<bool> &visited) {
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        cout << node << " "; 
+
+        for(int neighbour : adj[node]) {
+            if(!visited[neighbour]) {
+                visited[neighbour] = true;
+                q.push(neighbour);
+            }
+        }
+    }
+}
